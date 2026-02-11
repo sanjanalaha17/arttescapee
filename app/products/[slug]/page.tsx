568806a935ceaca116d38e0696e5e433d8476f2e
unknown <hrishikeshblaster@gmail.com>
2026-02-12 01:53:41 +0530

    third commit

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -56,8 +56,27 @@ const ProductPage = () => {
 
     const [productData, setProductData] = useState<productDataType>(initValues)
 
-    const handlePayment = async () => {
+    const loadRazorpay = () => {
+        return new Promise((resolve) => {
+            const script = document.createElement("script");
+            script.src = "https://checkout.razorpay.com/v1/checkout.js";
+            script.onload = () => {
+                resolve(true);
+            };
+            script.onerror = () => {
+                resolve(false);
+            };
+            document.body.appendChild(script);
+        });
+    };
 
+    const handlePayment = async () => {
+        const isLoaded = await loadRazorpay();
+
+        if (!isLoaded) {
+            alert("Razorpay SDK failed to load.");
+            return;
+        }
         const { data, status } = await axios.post('/api/create-payment', {
             order_amount,
         })
